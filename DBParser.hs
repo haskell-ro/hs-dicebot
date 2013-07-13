23bf1a622308296710bf87d9a52ef820e19d5e5e
Lucian Mogosanu <lucian.mogosanu@gmail.com>
2013-07-13 12:52:23 +0300

    DBParser: start and stop cmds

--- DBParser.hs
+++ DBParser.hs
@@ -1,4 +1,19 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
 module DBParser where
 
+import Text.Parsec
+
+import Types
+
 parseCmd :: String -> DiceBotCmd
-parseCmd = undefined
+parseCmd s = case parse cmd "(unknown)" s of
+  Right c -> c
+  _       -> Unknown
+
+cmd = do
+  bang
+  start <|> quit
+
+bang  = char '!'
+start = char 's' >> mapM_ (optional . char) "tart" >> eof >> return Start
+quit = char 'q' >> mapM_ (optional . char) "uit" >> eof >> return Quit
