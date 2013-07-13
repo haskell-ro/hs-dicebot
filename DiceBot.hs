53b9c276efe87242d4795c374e769284c7c3267d
Lucian Mogosanu <lucian.mogosanu@gmail.com>
2013-07-13 13:13:09 +0300

    DiceBot: support start and stop commands

--- DiceBot.hs
+++ DiceBot.hs
@@ -1,10 +1,13 @@
 module Main where
 
 import Network
-import qualified Network.IRC as IRC
 import System.IO
 import Text.Printf
 import Control.Monad (forever)
+import qualified Network.IRC as IRC
+
+import Types
+import DBParser
 
 server      = "irc.freenode.org"
 port        = 6667
@@ -33,10 +36,27 @@ listen :: Handle -> IO ()
 listen h = forever $ do
   s <- hGetLine h
   dbgIn s
+  --dbgIn $ show $ IRC.decode s
+  case IRC.decode s of
+    Nothing -> return ()
+    Just m  -> if IRC.msg_command m /= "PRIVMSG" then return ()
+        else do
+        let msg = (!! 1) . IRC.msg_params $ m
+            c = parseCmd msg
+        respond h c
+
+respond :: Handle -> DiceBotCmd -> IO ()
+respond h c = case c of
+  Start   -> write h $ IRC.privmsg chan "--- Session start ---"
+  Quit    -> write h $ IRC.privmsg chan "--- Session quit ---"
+  Roll _  -> write h $ IRC.privmsg chan "--- Unimplemented ---"
+  _       -> return ()
 
 -- debugging utilities
 dbgOut :: String -> IO ()
 dbgOut s = putStrLn $ "out> " ++ s
+-- dbgOut s = return ()
 
 dbgIn :: String -> IO ()
 dbgIn s = putStrLn $ "in> " ++ s
+-- dbgIn s = return ()
