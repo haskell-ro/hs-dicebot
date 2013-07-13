a896c4c46100ed7f8d88fa5fb0b203d4b5160f26
Lucian Mogosanu <lucian.mogosanu@gmail.com>
2013-07-13 13:44:14 +0300

    DBParser: implement constants

--- DBParser.hs
+++ DBParser.hs
@@ -12,8 +12,19 @@ parseCmd s = case parse cmd "(unknown)" s of
 
 cmd = do
   bang
-  start <|> quit
+  start <|> quit <|> roll
 
 bang  = char '!'
 start = char 's' >> mapM_ (optional . char) "tart" >> eof >> return Start
 quit = char 'q' >> mapM_ (optional . char) "uit" >> eof >> return Quit
+
+roll = do
+  char 'r' >> mapM_ (optional . char) "oll"
+  spaces
+  d <- die
+  eof
+  return $ Roll [d]
+
+die = constVal <|> dieVal
+constVal = alphaNum `sepEndBy1` spaces >>= return . Const . read
+dieVal = return $ Die 2 42
