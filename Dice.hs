325a28b6e9499a55ffc6e1f5a5505d5b12f0fb5f
Lucian Mogosanu <lucian.mogosanu@gmail.com>
2013-07-14 09:55:23 +0300

    Dice: add die roller

--- Dice.hs
+++ Dice.hs
@@ -1,5 +1,8 @@
 module Dice where
 
+import Control.Monad (liftM)
+import Control.Monad.Random
+
 data DiceBotCmd =
     Start
   | Quit
@@ -20,3 +23,19 @@ showDice (d : ds)
   where
   showDie (Const n) = show n
   showDie (Die n t) = show n ++ "d" ++ show t
+
+showResult :: [Int] -> String
+showResult [] = ""
+showResult (n : ns)
+  | null ns = show n
+  | otherwise = show n ++ " + " ++ showResult ns
+
+randomFace :: RandomGen g => Int -> Rand g Int
+randomFace t = getRandomR (1, t)
+
+rollDie :: RandomGen g => Die -> Rand g [Int]
+rollDie (Const n) = return [n]
+rollDie (Die n t) = sequence . replicate n $ randomFace t
+
+rollDice :: RandomGen g => [Die] -> Rand g [Int]
+rollDice = liftM concat . mapM rollDie
