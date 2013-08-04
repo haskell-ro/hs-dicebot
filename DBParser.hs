49070609937c4b11aee6e801d62348930df55ad6
Lucian Mogosanu <lucian.mogosanu@gmail.com>
2013-08-04 21:46:36 +0300

    DBParser: check dice sanity
    Closes #4 and #7.

--- DBParser.hs
+++ DBParser.hs
@@ -23,7 +23,9 @@ roll = do
   spaces
   ds <- sumDice
   eof
-  return $ Roll ds
+  return $ case checkDice ds of
+    Left err  -> Bad err
+    Right ds  -> Roll ds
 
 sumDice = do
   d <- die
@@ -31,12 +33,28 @@ sumDice = do
   return (d : ds)
 
 die = try dieVal <|> constVal
-constVal = digit `sepEndBy1` spaces >>= return . Const . read
+constVal = digit `sepEndBy1` spaces >>= return . Right . Const . read
 dieVal = do
   dn <- number <|> return 1
   char 'd'
   spaces
   dt <- number <|> return 6
-  return $ Die dn dt
+  return . checkDie $ Die dn dt
   where
   number = fmap read $ digit `sepEndBy1` spaces
+
+-- semantic hack: sanity checking for dice
+checkDie :: Die -> Either String Die
+checkDie d@(Die n t)
+  | n == 0 || t == 0 || n > 20 || t > 100 = Left "invalid roll"
+  | otherwise                             = Right d
+
+checkDice :: [Either String Die] -> Either String [Die]
+checkDice eds = case filter isError eds of
+  []            -> Right . map right $ eds
+  Left err : _  -> Left err
+  where
+  isError (Left _)  = True
+  isError _         = False
+  right (Right r) = r
+  right _         = error "Either: Left encountered"
